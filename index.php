5aaf7a29789f67fed0be95ccf3acd1f1273247ee
Ricardo <ricardo.remond.pro@gmail.com>
2023-12-20 16:17:18 +0100

    fin de journee

--- index.php
+++ index.php
@@ -9,7 +9,40 @@
 </head>
 
 <body>
-    <header></header>
+    <header>
+        <div class="navbarTop">
+            <img src="./assets/img/logo/logoAfpa.jpg" alt="Logo Afpa">
+            <div class="link">
+                <a href="#">Trouver votre centre</a>
+                <a href="#"> Vos questions </a>
+            </div>
+            <button class="compte" type="button">Mon compte</button>
+        </div>
+        <div class="navbarLink">
+
+            <p>Je suis</p>
+            <p>
+                <a href="#">Particulier</a>
+            </p>
+            <p>
+                <a href="#">Partenaire</a>
+            </p>
+            <p>
+                <a href="#">Entreprise</a>
+            </p>
+            <p>
+                <a href="#">Nos formations</a>
+            </p>
+            <p>
+                <a href="#">Actualités</a>
+            </p>
+            <p>
+                <a href="#">Groupe Apfa</a>
+            </p>
+        </div>
+
+
+    </header>
 </body>
 
 </html>
