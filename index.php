2990f74bdaf7ff778de153d6b9e6759d4e616027
Ricardo <ricardo.remond.pro@gmail.com>
2023-12-22 12:45:51 +0100

    css ne marche plus sa fait chier !

--- index.php
+++ index.php
@@ -18,6 +18,71 @@
     include("./components/headerNavbar.php");
     ?>
 
+    <?php
+
+    include('./assets/data/data.php');
+
+    $images = [
+        "./assets/img/portrait-1.jpg",
+        "./assets/img/portrait-2.jpg",
+        "./assets/img/portrait-3.jpg",
+        "./assets/img/portrait-4.jpg",
+        "./assets/img/portrait-5.jpg",
+        "./assets/img/portrait-6.jpg",
+        "./assets/img/portrait-7.jpg",
+        "./assets/img/portrait-8.jpg",
+        "./assets/img/portrait-9.jpg",
+        "./assets/img/portrait-10.jpg",
+        "./assets/img/portrait-11.jpg",
+        "./assets/img/portrait-12.jpg",
+        "./assets/img/portrait-13.jpg",
+        "./assets/img/portrait-14.jpg",
+        "./assets/img/portrait-15.jpg",
+        "./assets/img/portrait-16.jpg",
+        "./assets/img/portrait-17.jpg",
+        "./assets/img/portrait-18.jpg",
+        "./assets/img/portrait-19.jpg",
+        "./assets/img/portrait-20.jpg",
+    ];
+
+    $imageIndex = 0;
+    ?>
+
+
+    <?php
+    $path_to_data = './assets/data/data.php';
+    include($path_to_data);
+    ?>
+
+
+    <div class="boucleAlumni">
+        <?php foreach ($data as $person): ?>
+            <div class="carteAlumni">
+                <h3>
+                    <?= $person['name'] ?>
+                </h3>
+                <img src="<?= $images[$imageIndex] ?>" alt="Portrait">
+                <p>Numéro de téléphone :
+                    <?= $person['phone'] ?>
+                </p>
+                <p>Email:
+                    <?= $person['email'] ?>
+                </p>
+                <p>Region:
+                    <?= $person['region'] ?>
+                </p>
+            </div>
+            <?php
+            $imageIndex = ($imageIndex + 1) % count($images);
+
+            ?>
+        <?php endforeach; ?>
+    </div>
+
+
+
+
+
 
 </body>
 
